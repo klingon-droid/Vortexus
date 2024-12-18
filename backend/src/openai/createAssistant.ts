37c8b50abc99606d70f4003edb0d7694c3dba055
Damjan Dimitrov <damjandimitrov1@gmail.com>
2024-12-18 14:27:33 +0100

    Implement meteora pool action

--- backend/src/openai/createAssistant.ts
+++ backend/src/openai/createAssistant.ts
@@ -4,7 +4,7 @@ import { tools } from '../tools/allTools.ts';
 import { assistantPrompt } from '../const/prompt.ts';
 
 export async function createAssistant(client: OpenAI): Promise<Assistant> {
-  return await client.beta.assistants.retrieve(process.env.ASSISTANT_ID);
+  // return await client.beta.assistants.retrieve(process.env.ASSISTANT_ID);
 
   return await client.beta.assistants.create({
     model: 'gpt-4o-mini',
