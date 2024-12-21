92cd9f8b1b0dfbeeff59c37bbe98836fcc61fd9a
Damjan <damjandimitrov1@gmail.com>
2024-12-21 17:27:43 +0100

    Update text

--- frontend/app/components/writer.tsx
+++ frontend/app/components/writer.tsx
@@ -25,19 +25,16 @@ export function TypewriterEffectSmoothDemo() {
   ];
   return (
     <div className="flex flex-col items-center justify-center h-[40rem]  ">
-      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-        The road to freedom starts from here
-      </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
         <Link href="/Chat">
           <button className="w-40 h-10 rounded-xl bg-violet-500 border dark:border-white border-transparent text-white text-sm">
-            Arcturus Web agent
+            Arcturus Web
           </button>
         </Link>
         <Link href="https://t.me/arcturusAI_bot">
           <button className="w-40 h-10 rounded-xl bg-violet-500 text-white border border-gray-300  text-sm">
-            Arcturus Telegram agent
+            Arcturus Telegram
           </button>
         </Link>
       </div>
